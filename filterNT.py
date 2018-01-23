72b810b33f21baa8b17f0b3f8052a0b4cd91c351
john <jsh58@wildcats.unh.edu>
2018-01-23 12:36:28 -0500

    filterNT.py: excluding specified sequences

--- filterNT.py
+++ filterNT.py
@@ -41,23 +41,14 @@ def openWrite(filename):
     sys.exit(-1)
   return f
 
-def main():
-  args = sys.argv[1:]
-  if len(args) < 2:
-    sys.stderr.write('Usage: python filterNT.py  <input>  <output>  [<minLen>]\n')
-    sys.stderr.write('  <minLen>    Minimum sequence length (def. 25bp)\n')
-    sys.exit(-1)
-
-  fIn = openRead(args[0])
-  fOut = openWrite(args[1])
-  minLen = 25
-  if len(args) > 2:
-    minLen = int(args[2])
-
-  # parse fasta, write output on the fly
-  count = short = pureNs = 0
-  head = ''
-  seq = ''
+def parseFasta(fIn, fOut, minLen, headers):
+  '''
+  Parse fasta file, write output on the fly.
+  '''
+  count = short = pureNs = xReads = 0
+  head = ''  # header (1st space-delim token)
+  read = ''  # full read (header + sequence)
+  seq = ''   # sequence (no newlines)
   for line in fIn:
     if line[0] == '>':
       if seq:
@@ -66,12 +57,15 @@ def main():
           short += 1
         elif seq == 'N' * len(seq):
           pureNs += 1
+        elif head in headers:
+          xReads += 1
         else:
-          fOut.write(head)
+          fOut.write(read)
         seq = ''
-      head = line
-    elif head:
-      head += line
+      read = line
+      head = line.split(' ')[0][1:]
+    elif read:
+      read += line
       seq += line.rstrip()
   if fIn != sys.stdin:
     fIn.close()
@@ -81,13 +75,42 @@ def main():
       short += 1
     elif seq == 'N' * len(seq):
       pureNs += 1
+    elif head in headers:
+      xReads += 1
     else:
-      fOut.write(head)
+      fOut.write(read)
+  return count, short, pureNs, xReads
+
+def main():
+  '''Main.'''
+  args = sys.argv[1:]
+  if len(args) < 2:
+    sys.stderr.write('Usage: python filterNT.py  <input>  ' \
+      + '<output>  [<minLen>]  [<headers]\n')
+    sys.stderr.write('  <minLen>    Minimum sequence length (def. 25bp)\n')
+    sys.stderr.write('  <headers>   File of read headers to ignore\n')
+    sys.exit(-1)
+
+  fIn = openRead(args[0])
+  fOut = openWrite(args[1])
+  minLen = 25
+  if len(args) > 2:
+    minLen = int(args[2])
+  headers = []
+  if len(args) > 3:
+    fRead = openRead(args[3])
+    for line in fRead:
+      headers.append(line.rstrip())
+
+  # parse fasta
+  count, short, pureNs, xReads = parseFasta(fIn, fOut, minLen, headers)
 
   sys.stderr.write('Total fasta sequences in %s: %d\n' % (args[0], count))
   sys.stderr.write('  Shorter than %dbp: %d\n' % (minLen, short))
   sys.stderr.write('  Pure Ns: %d\n' % pureNs)
-  sys.stderr.write('  Written to %s: %d\n' % (args[1], count - short - pureNs))
+  sys.stderr.write('  Excluded : %d\n' % xReads)
+  sys.stderr.write('  Written to %s: ' % args[1] \
+    + '%d\n' % (count - short - pureNs - xReads))
 
 if __name__ == '__main__':
   main()
